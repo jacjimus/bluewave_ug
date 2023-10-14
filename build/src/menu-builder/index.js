bd42df3d4f126f50231cb93b85c83df7a7f7cb36
DickensJuma <dickensjuma13@gmail.com>
2023-10-14 13:51:18 +0300

    famili menu

--- build/src/menu-builder/index.js
+++ build/src/menu-builder/index.js
@@ -17,6 +17,7 @@ const configs_1 = __importDefault(require("./configs"));
 const ussd_menu_builder_1 = __importDefault(require("ussd-menu-builder"));
 const db_1 = require("../models/db");
 const selfMenu_1 = __importDefault(require("./menus/selfMenu"));
+const familyMenu_1 = __importDefault(require("./menus/familyMenu"));
 require("dotenv").config();
 const Session = db_1.db.sessions;
 const User = db_1.db.users;
@@ -78,112 +79,10 @@ function default_1(args, db) {
             }
             else if (firstStep == "1") {
                 response = yield (0, selfMenu_1.default)(params, db);
-                //     break;
-                //   case "2":
-                //     response = "Buy for family " +
-                //       "\n1. Self + Spouse or Child" +
-                //       "\n2. Self + Spouse + 1 Child" +
-                //       "\n3. Self + Spouse + 2 Children" +
-                //       "\n01. Next"
-                //     break;
-                //   case "3":
-                //     response = "CON Buy for others " +
-                //       "\n1. Other " +
-                //       "\n2. Other + Spouse or Child" +
-                //       "\n3. Other + Spouse + 1 Children" +
-                //       "\n01. Next"
-                //     break;
-                //   case "4":
-                //     response = "CON Make Claim " +
-                //       "\n1. Inpatient Claim" +
-                //       "\n2. Death Claim"
-                //     break;
-                //   case "5":
-                //     response = "CON My Account" +
-                //       "\n1. Policy Status" +
-                //       "\n2. Pay Now" +
-                //       "\n3. Renew Policy" +
-                //       "\n4. Update My Profile (KYC)" +
-                //       "\n5. Cancel Policy" +
-                //       "\n6. Add Dependant" +
-                //       "\n7. My Hospital"
-                //     break;
-                //   case "6":
-                //     const regions = [
-                //       "Central Region",
-                //       "Western Region",
-                //       "Eastern Region",
-                //       "Karamoja Region",
-                //       "West Nile Region",
-                //       "Northern Region",
-                //     ];
-                //     response = "CON "
-                //     regions.forEach((region, index) => {
-                //       response += `${index + 1}. ${region}\n`;
-                //     });
-                //     break;
-                //   case "7":
-                //     response = 'END To view Medical cover Terms &Conditions Visit www.tclink.com '
-                //     break;
-                //   case "8":
-                //     response = "CON FAQs " +
-                //       "\n1. Eligibility" +
-                //       "\n2. Mini cover" +
-                //       "\n3. Midi Cover" +
-                //       "\n4. Biggie cover" +
-                //       "\n5. Waiting period" +
-                //       '\n6. Waiting period meaning' +
-                //       "\n7. When to make claim" +
-                //       "\n8. Claim Payment" +
-                //       '\n9. Renewal' +
-                //       "\n99. Insured Name";
-                //     break;
-                //   default:
-                //     response = "END " + languages[configs.default_lang].generic.invalid_option;
-                //     break;
-                // }
             }
-            // else if (currentStep == 2) {
-            //   let coverType = "";
-            //   let sum_insured = "";
-            //   let premium = "";
-            //   let yearly_premium = "";
-            //   switch (userText) {
-            //     case "1":
-            //       coverType = "MINI";
-            //       sum_insured = "1.5M"
-            //       premium = "10,000";
-            //       yearly_premium = "120,000";
-            //       response = `CON Inpatient cover for ${args.phoneNumber}, UGX ${sum_insured} a year` +
-            //         "\nPAY:" +
-            //         `\n1-UGX ${premium} monthly` +
-            //         `\n2-UGX ${yearly_premium} yearly`
-            //       break;
-            //     case "2":
-            //       coverType = "MIDI";
-            //       sum_insured = "3M"
-            //       premium = "14,000";
-            //       yearly_premium = "167,000";
-            //       response = `CON Inpatient cover for ${args.phoneNumber}, UGX ${sum_insured} a year` +
-            //         "\nPAY" +
-            //         `\n1-UGX ${premium} monthly` +
-            //         `\n2-UGX ${yearly_premium} yearly`
-            //       break;
-            //     case "3":
-            //       coverType = "BIGGIE";
-            //       sum_insured = "5M"
-            //       premium = "18,000";
-            //       yearly_premium = "208,000";
-            //       response = `CON Inpatient cover for ${args.phoneNumber}, UGX ${sum_insured} a year` +
-            //         "\nPAY" +
-            //         `\n1-UGX ${premium} monthly` +
-            //         `\n2-UGX ${yearly_premium} yearly`
-            //       break;
-            //     default:
-            //       response = "END Invalid option";
-            //       break;
-            //   }
-            // }
+            else if (firstStep == "2") {
+                response = yield (0, familyMenu_1.default)(params, db);
+            }
             resolve(response);
             return;
         }
--- src/menu-builder/index.ts
+++ src/menu-builder/index.ts
@@ -19,6 +19,7 @@ import { airtelMoney } from "../services/payment";
 import { db } from "../models/db";
 import { buyForOthers } from "./menus/buyForOthers";
 import selfMenu from "./menus/selfMenu";
+import familyMenu from "./menus/familyMenu";
 
 require("dotenv").config();
 
@@ -93,116 +94,11 @@ export default function (args: RequestBody, db: any) {
           "\n8. FAQs"
       }
       else if (firstStep == "1") {
-          response = await selfMenu(params, db);
-
-
-        //     break;
-        //   case "2":
-        //     response = "Buy for family " +
-        //       "\n1. Self + Spouse or Child" +
-        //       "\n2. Self + Spouse + 1 Child" +
-        //       "\n3. Self + Spouse + 2 Children" +
-        //       "\n01. Next"
-        //     break;
-        //   case "3":
-        //     response = "CON Buy for others " +
-        //       "\n1. Other " +
-        //       "\n2. Other + Spouse or Child" +
-        //       "\n3. Other + Spouse + 1 Children" +
-        //       "\n01. Next"
-        //     break;
-        //   case "4":
-        //     response = "CON Make Claim " +
-        //       "\n1. Inpatient Claim" +
-        //       "\n2. Death Claim"
-        //     break;
-        //   case "5":
-        //     response = "CON My Account" +
-        //       "\n1. Policy Status" +
-        //       "\n2. Pay Now" +
-        //       "\n3. Renew Policy" +
-        //       "\n4. Update My Profile (KYC)" +
-        //       "\n5. Cancel Policy" +
-        //       "\n6. Add Dependant" +
-        //       "\n7. My Hospital"
-        //     break;
-        //   case "6":
-        //     const regions = [
-        //       "Central Region",
-        //       "Western Region",
-        //       "Eastern Region",
-        //       "Karamoja Region",
-        //       "West Nile Region",
-        //       "Northern Region",
-        //     ];
-        //     response = "CON "
-        //     regions.forEach((region, index) => {
-        //       response += `${index + 1}. ${region}\n`;
-        //     });
-        //     break;
-        //   case "7":
-        //     response = 'END To view Medical cover Terms &Conditions Visit www.tclink.com '
-        //     break;
-        //   case "8":
-        //     response = "CON FAQs " +
-        //       "\n1. Eligibility" +
-        //       "\n2. Mini cover" +
-        //       "\n3. Midi Cover" +
-        //       "\n4. Biggie cover" +
-        //       "\n5. Waiting period" +
-        //       '\n6. Waiting period meaning' +
-        //       "\n7. When to make claim" +
-        //       "\n8. Claim Payment" +
-        //       '\n9. Renewal' +
-        //       "\n99. Insured Name";
-        //     break;
-        //   default:
-        //     response = "END " + languages[configs.default_lang].generic.invalid_option;
-        //     break;
-        // }
+        response = await selfMenu(params, db);
       }
-      // else if (currentStep == 2) {
-      //   let coverType = "";
-      //   let sum_insured = "";
-      //   let premium = "";
-      //   let yearly_premium = "";
-      //   switch (userText) {
-      //     case "1":
-      //       coverType = "MINI";
-      //       sum_insured = "1.5M"
-      //       premium = "10,000";
-      //       yearly_premium = "120,000";
-      //       response = `CON Inpatient cover for ${args.phoneNumber}, UGX ${sum_insured} a year` +
-      //         "\nPAY:" +
-      //         `\n1-UGX ${premium} monthly` +
-      //         `\n2-UGX ${yearly_premium} yearly`
-      //       break;
-      //     case "2":
-      //       coverType = "MIDI";
-      //       sum_insured = "3M"
-      //       premium = "14,000";
-      //       yearly_premium = "167,000";
-      //       response = `CON Inpatient cover for ${args.phoneNumber}, UGX ${sum_insured} a year` +
-      //         "\nPAY" +
-      //         `\n1-UGX ${premium} monthly` +
-      //         `\n2-UGX ${yearly_premium} yearly`
-      //       break;
-      //     case "3":
-      //       coverType = "BIGGIE";
-      //       sum_insured = "5M"
-      //       premium = "18,000";
-      //       yearly_premium = "208,000";
-      //       response = `CON Inpatient cover for ${args.phoneNumber}, UGX ${sum_insured} a year` +
-      //         "\nPAY" +
-      //         `\n1-UGX ${premium} monthly` +
-      //         `\n2-UGX ${yearly_premium} yearly`
-      //       break;
-      //     default:
-      //       response = "END Invalid option";
-      //       break;
-      //   }
-      // }
-
+      else if (firstStep == "2") {
+        response = await familyMenu(params, db);
+      }
       resolve(response);
 
 
