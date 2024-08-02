8efa810840ddd60ce3986548de6f03f5580879bd
James Makau <jacjimus@gmail.com>
2024-08-02 16:51:54 +0300

    added dlay of 5 secs

--- src/routes/ussdRoutes.ts
+++ src/routes/ussdRoutes.ts
@@ -27,7 +27,7 @@ const sendResponse = (res, mode, data) => {
     res.setHeader('amount', '0');
   }
   res.setHeader('content-type', 'text/plain');
-  //data = data.replace(/^(CON |END )/g, "").trim();
+  data = data.replace(/^(CON |END )/g, "").trim();
   res.send(data);
 };
 
